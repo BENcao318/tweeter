d2be9bb49626921eaeeafc14a3fef19b2768ebba
BENcao318 <labber@DESKTOP-U16DFJJ.localdomain>
2022-02-26 20:09:35 -0500

    add new tweet section and css effects

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $(".new-tweet form textarea").on('keyup', function() {
+    let counter = $(this).parents('form').find('div .counter');
+    let charLeft = 140 - $(this).val().length;
+    counter.val(charLeft);
+    if(charLeft < 0) {
+      counter.css("color", "red");
+    }
+  })
+
+});
